b19e31c1ae1571fd2341103b9539ebbe81b33834
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2024-12-16 03:48:34 -0500

    correct and complete BinarySearchUtils

--- puzzles-leet-code/src/main/java/org/krmdemo/techlabs/utils/BinarySearchUtils.java
+++ puzzles-leet-code/src/main/java/org/krmdemo/techlabs/utils/BinarySearchUtils.java
@@ -1,9 +1,22 @@
 package org.krmdemo.techlabs.utils;
 
-import java.util.*;
-
+/**
+ * Utility-Class to perform binary-search not to find the given value,
+ * but count the elements that are greater or less than given value
+ */
 public class BinarySearchUtils {
 
+    /**
+     * Count the number of elements in the sub-array <code>[left,right)</code>
+     * of the passed sorted array <code>arr</code>
+     * that are <b>strictly less</b> than the passed <code>ceilValue</code>
+     *
+     * @param arr an array of integers that is sorted in ascending (natural) order
+     * @param left the left boundary of sub-array (inclusive)
+     * @param right the right boundary of sub-array (exclusive)
+     * @param ceilValue the upper-value (<b>exclusive</b>)
+     * @return the number of elements that are <b>strictly less</b> than <code>ceilValue</code>
+     */
     public static int countLess(int[] arr, int left, int right, int ceilValue) {
         left = Math.max(0, left);
         right = Math.min(right, arr.length);
@@ -13,6 +26,7 @@ public class BinarySearchUtils {
         if (arr[right - 1] < ceilValue) {
             return right - left;
         }
+        int initialLeft = left;
         while (left + 1 < right) {
             int mid = (left + right) / 2;
             int midValue = arr[mid];
@@ -22,9 +36,20 @@ public class BinarySearchUtils {
                 left = mid;
             }
         }
-        return left + 1;
+        return left + 1 - initialLeft;
     }
 
+    /**
+     * Count the number of elements in the sub-array <code>[left,right)</code>
+     * of the passed sorted array <code>arr</code>
+     * that are equal or less (<b>not greater</b>) than the passed <code>ceilValue</code>
+     *
+     * @param arr an array of integers that is sorted in ascending (natural) order
+     * @param left the left boundary of sub-array (inclusive)
+     * @param right the right boundary of sub-array (exclusive)
+     * @param ceilValue the upper-value (<b>inclusive</b>)
+     * @return the number of elements that are <b>not greater</b> than <code>ceilValue</code>
+     */
     public static int countNotGreater(int[] arr, int left, int right, int ceilValue) {
         left = Math.max(0, left);
         right = Math.min(right, arr.length);
@@ -34,6 +59,7 @@ public class BinarySearchUtils {
         if (arr[right - 1] <= ceilValue) {
             return right - left;
         }
+        int initialLeft = left;
         while (left + 1 < right) {
             int mid = (left + right) / 2;
             int midValue = arr[mid];
@@ -43,9 +69,20 @@ public class BinarySearchUtils {
                 left = mid;
             }
         }
-        return left + 1;
+        return left + 1 - initialLeft;
     }
 
+    /**
+     * Count the number of elements in the sub-array <code>[left,right)</code>
+     * of the passed sorted array <code>arr</code>
+     * that are <b>strictly greater</b> than the passed <code>floorValue</code>
+     *
+     * @param arr an array of integers that is sorted in ascending (natural) order
+     * @param left the left boundary of sub-array (inclusive)
+     * @param right the right boundary of sub-array (exclusive)
+     * @param floorValue the lower-value (<b>exclusive</b>)
+     * @return the number of elements that are <b>strictly greater</b> than <code>floorValue</code>
+     */
     public static int countGreater(int[] arr, int left, int right, int floorValue) {
         left = Math.max(0, left);
         right = Math.min(right, arr.length);
@@ -68,10 +105,21 @@ public class BinarySearchUtils {
         return initialRight - right;
     }
 
+    /**
+     * Count the number of elements in the sub-array <code>[left,right)</code>
+     * of the passed sorted array <code>arr</code>
+     * that are equal or greater (<b>not less</b>) than the passed <code>floorValue</code>
+     *
+     * @param arr an array of integers that is sorted in ascending (natural) order
+     * @param left the left boundary of sub-array (inclusive)
+     * @param right the right boundary of sub-array (exclusive)
+     * @param floorValue the lower-value (<b>inclusive</b>)
+     * @return the number of elements that are not less than <code>floorValue</code>
+     */
     public static int countNotLess(int[] arr, int left, int right, int floorValue) {
         left = Math.max(0, left);
         right = Math.min(right, arr.length);
-        if (left >= right || arr[right - 1] <= floorValue) {
+        if (left >= right || arr[right - 1] < floorValue) {
             return 0;
         }
         if (arr[left] >= floorValue) {
--- puzzles-leet-code/src/test/java/org/krmdemo/techlabs/utils/BinarySearchUtilsTest.java
+++ puzzles-leet-code/src/test/java/org/krmdemo/techlabs/utils/BinarySearchUtilsTest.java
@@ -3,7 +3,13 @@ package org.krmdemo.techlabs.utils;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit-test for {@link BinarySearchUtils}
+ */
 public class BinarySearchUtilsTest {
 
     @Test
@@ -15,6 +21,26 @@ public class BinarySearchUtilsTest {
             System.out.printf("ceilValue = %2d; cntLess = %d; %s %n",
                 i, cntLess, dumpPrefixArr(arr, cntLess));
         }
+        int[] cntLessArr_all = IntStream.rangeClosed(-2, 40)
+            .map(floorValue -> BinarySearchUtils.countLess(arr, 0, arr.length, floorValue))
+            .toArray();
+        assertThat(cntLessArr_all).containsExactly(
+            0, 0, 0,
+            0, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+            3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+            3, 3, 3, 3, 3, 4, 4, 4, 4, 4,
+            4, 5, 6, 7, 7, 7, 8, 8, 8, 8
+        );
+        int[] cntLessArr_mid = IntStream.rangeClosed(-2, 40)
+            .map(floorValue -> BinarySearchUtils.countLess(arr, 2, arr.length - 3, floorValue))
+            .toArray();
+        assertThat(cntLessArr_mid).containsExactly(
+            0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 2, 2, 2, 2, 2,
+            2, 3, 3, 3, 3, 3, 3, 3, 3, 3
+        );
     }
 
     @Test
@@ -22,10 +48,30 @@ public class BinarySearchUtilsTest {
         int[] arr = new int[] { 1, 3, 10, 25, 31, 32, 33, 36 };
         System.out.println("testCountNotGreater: " + Arrays.toString(arr));
         for (int i = -2; i <= 40; i++) {
-            int cntNG = BinarySearchUtils.countNotGreater(arr, 0, arr.length, i);
-            System.out.printf("ceilValue = %2d; cntNG = %d; %s %n",
-                i, cntNG, dumpPrefixArr(arr, cntNG));
+            int cntNotGreater = BinarySearchUtils.countNotGreater(arr, 0, arr.length, i);
+            System.out.printf("ceilValue = %2d; cntNotGreater = %d; %s %n",
+                i, cntNotGreater, dumpPrefixArr(arr, cntNotGreater));
         }
+        int[] cntNotGreaterArr_all = IntStream.rangeClosed(-2, 40)
+            .map(floorValue -> BinarySearchUtils.countNotGreater(arr, 0, arr.length, floorValue))
+            .toArray();
+        assertThat(cntNotGreaterArr_all).containsExactly(
+            0, 0, 0,
+            1, 1, 2, 2, 2, 2, 2, 2, 2, 3,
+            3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+            3, 3, 3, 3, 4, 4, 4, 4, 4, 4,
+            5, 6, 7, 7, 7, 8, 8, 8, 8, 8
+        );
+        int[] cntNotGreaterArr_mid = IntStream.rangeClosed(-2, 40)
+            .map(floorValue -> BinarySearchUtils.countNotGreater(arr, 2, arr.length - 3, floorValue))
+            .toArray();
+        assertThat(cntNotGreaterArr_mid).containsExactly(
+            0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 2, 2, 2, 2, 2, 2,
+            3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+        );
     }
 
     @Test
@@ -48,6 +94,26 @@ public class BinarySearchUtilsTest {
             System.out.printf("floorValue = %2d; cntGreater = %d; %s %n",
                 i, cntGreater, dumpSuffixArr(arr, cntGreater));
         }
+        int[] cntGreaterArr_all = IntStream.rangeClosed(-2, 40)
+            .map(floorValue -> BinarySearchUtils.countGreater(arr, 0, arr.length, floorValue))
+            .toArray();
+        assertThat(cntGreaterArr_all).containsExactly(
+            8, 8, 8,
+            7, 7, 6, 6, 6, 6, 6, 6, 6, 5,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 4, 4, 4, 4, 4, 4,
+            3, 2, 1, 1, 1, 0, 0, 0, 0, 0
+        );
+        int[] cntGreaterArr_mid = IntStream.rangeClosed(-2, 40)
+            .map(floorValue -> BinarySearchUtils.countGreater(arr, 2, arr.length - 3, floorValue))
+            .toArray();
+        assertThat(cntGreaterArr_mid).containsExactly(
+            3, 3, 3,
+            3, 3, 3, 3, 3, 3, 3, 3, 3, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 1, 1, 1, 1, 1, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        );
     }
 
     @Test
@@ -56,20 +122,40 @@ public class BinarySearchUtilsTest {
         System.out.println("testCountNotLess: " + Arrays.toString(arr));
         {
             int floorValue = 35;
-            int cntNL = BinarySearchUtils.countNotLess(arr, 0, arr.length, floorValue);
-            System.out.printf("*** floorValue = %2d; cntNL = %d; %s %n",
-                floorValue, cntNL, dumpSuffixArr(arr, cntNL));
+            int cntNotLess = BinarySearchUtils.countNotLess(arr, 0, arr.length, floorValue);
+            System.out.printf("*** floorValue = %2d; cntNotLess = %d; %s %n",
+                floorValue, cntNotLess, dumpSuffixArr(arr, cntNotLess));
         } {
             int floorValue = 36;
-            int cntNL = BinarySearchUtils.countNotLess(arr, 0, arr.length, floorValue);
-            System.out.printf("*** floorValue = %2d; cntNL = %d; %s %n",
-                floorValue, cntNL, dumpSuffixArr(arr, cntNL));
+            int cntNotLess = BinarySearchUtils.countNotLess(arr, 0, arr.length, floorValue);
+            System.out.printf("*** floorValue = %2d; cntNotLess = %d; %s %n",
+                floorValue, cntNotLess, dumpSuffixArr(arr, cntNotLess));
         }
-//        for (int i = -2; i <= 40; i++) {
-//            int cntNL = BinarySearchUtils.countNotLess(arr, 0, arr.length, i);
-//            System.out.printf("floorValue = %2d; cntNL = %d; %s %n",
-//                i, cntNL, dumpSuffixArr(arr, cntNL));
-//        }
+        for (int i = -2; i <= 40; i++) {
+            int cntNotLess = BinarySearchUtils.countNotLess(arr, 0, arr.length, i);
+            System.out.printf("floorValue = %2d; cntNotLess = %d; %s %n",
+                i, cntNotLess, dumpSuffixArr(arr, cntNotLess));
+        }
+        int[] cntNotLessArr_all = IntStream.rangeClosed(-2, 40)
+            .map(floorValue -> BinarySearchUtils.countNotLess(arr, 0, arr.length, floorValue))
+            .toArray();
+        assertThat(cntNotLessArr_all).containsExactly(
+            8, 8, 8,
+            8, 7, 7, 6, 6, 6, 6, 6, 6, 6,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 4, 4, 4, 4, 4,
+            4, 3, 2, 1, 1, 1, 0, 0, 0, 0
+        );
+        int[] cntNotLessArr_mid = IntStream.rangeClosed(-2, 40)
+            .map(floorValue -> BinarySearchUtils.countNotLess(arr, 2, arr.length - 3, floorValue))
+            .toArray();
+        assertThat(cntNotLessArr_mid).containsExactly(
+            3, 3, 3,
+            3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        );
     }
 
     private String dumpPrefixArr(int[] arr, int prefixLength) {
