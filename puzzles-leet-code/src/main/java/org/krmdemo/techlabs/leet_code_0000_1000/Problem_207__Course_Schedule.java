ff79488b39a50a9510250ba8320e64bdd2f8d203
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2024-12-18 09:03:13 -0500

    Problem_207__Course_Schedule

---
+++ puzzles-leet-code/src/main/java/org/krmdemo/techlabs/leet_code_0000_1000/Problem_207__Course_Schedule.java
@@ -0,0 +1,178 @@
+package org.krmdemo.techlabs.leet_code_0000_1000;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * <h3><a href="https://leetcode.com/problems/course-schedule/description/?envType=problem-list-v2&envId=topological-sort">
+ *     207. Course Schedule
+ * </a></h3>
+ * There are a total of <b><code>numCourses</code></b> courses you have to take,
+ * labeled from <code>0</code> to <code>numCourses - 1</code>.
+ * You are given an array <b><code>prerequisites</code></b>
+ * where <code>prerequisites[<b>i</b>] = [a<b>i</b>, b<b>i</b>]</code>
+ * indicates that you must take course <code>b<b>i</b></code> first if you want to take course <code>a<b>i</b></code>.
+ * <hr/>
+ * For example, the pair <code>[0, 1]</code>, indicates that to take course <code>0</code>
+ * you have to first take course <code>1</code>.
+ * <hr/>
+ * Return <code>true</code> if you can finish all courses. Otherwise, return <code>false</code>.
+ * <h5>Constraints:</h5><pre>
+ * 1 <= numCourses <= 2000
+ * 0 <= prerequisites.length <= 5000
+ * prerequisites[i].length == 2
+ * 0 <= ai, bi < numCourses</pre>
+ * All the pairs <code>prerequisites[i]</code> are unique.
+ *
+ * @see <a href="https://www.geeksforgeeks.org/topological-sorting/">
+ *    (GFG) Topological Sorting
+ * </a>
+ */
+public interface Problem_207__Course_Schedule {
+
+    /**
+     * Solution entry-point.
+     *
+     * @param numCourses total number of courses you have to take
+     * @param prerequisites pair of pre-requisite conditions (the first must precede the second)
+     * @return <code>true</code> if you can finish all courses. Otherwise, return <code>false</code>
+     */
+    boolean canFinish(int numCourses, int[][] prerequisites);
+
+    enum Solution implements Problem_207__Course_Schedule {
+        DEFAULT;  // <-- one by one topological deletion is not efficient - TODO: try to delete the whole level
+
+        @Override
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            if (numCourses <= 0) {
+                throw new IllegalArgumentException(
+                    "number of courses must be positive, but it equals to " + numCourses);
+            }
+            CoursesGraph coursesGraph = new CoursesGraph(numCourses);
+            coursesGraph.addEdgesArr(prerequisites);
+            System.out.println("before topological sort:");
+            System.out.println(coursesGraph);
+            while (coursesGraph.hasFirstToRemove()) {
+                coursesGraph.removeFirst();
+            }
+            System.out.println("after topological sort:");
+            System.out.println(coursesGraph);
+            System.out.println("returning " + coursesGraph.isEmpty());
+            System.out.println("===================================");
+            return coursesGraph.isEmpty();
+        }
+
+        private static class CoursesGraph {
+            private final Node[] nodes;
+            private int size = 0;
+            CoursesGraph(int numCourses) {
+                this.nodes = new Node[numCourses];
+            }
+            record Node (int id, BitSet before, BitSet after) {
+                Node(int id) {
+                    this(id, new BitSet(), new BitSet());
+                }
+                int countBefore() {
+                    return before.cardinality();
+                }
+                boolean hasBefore() {
+                    return countBefore() == 0;
+                }
+                @Override
+                public String toString() {
+                    return String.format("#%d --> before: %s, after: %s;",
+                        this.id(), this.before(), this.after());
+                }
+            }
+            Node node(int id) {
+                if (nodes[id] == null) {
+                    nodes[id] = new Node(id);
+                    size++;
+                }
+                return nodes[id];
+            }
+            boolean isEmpty() {
+                return size == 0;
+            }
+
+            private static final Comparator<Node> topologicalBefore =
+                Comparator.comparingInt(Node::countBefore)
+                    .thenComparingInt(Node::id);
+            private final NavigableSet<Node> topologicalSet =
+                new TreeSet<>(topologicalBefore);
+
+            public void addEdge(int[] pair) {
+                Node nodeBefore = node(pair[0]);
+                Node nodeAfter = node(pair[1]);
+                topologicalSet.remove(nodeBefore);
+                topologicalSet.remove(nodeAfter);
+                nodeBefore.after.set(nodeAfter.id());
+                nodeAfter.before.set(nodeBefore.id());
+                topologicalSet.add(nodeBefore);
+                topologicalSet.add(nodeAfter);
+            }
+            public void addEdgesArr(int[][] pairsArr) {
+                if (pairsArr != null) {
+                    Arrays.stream(pairsArr).forEach(this::addEdge);
+                }
+            }
+            public Optional<Node> topologicalFirst() {
+                return topologicalSet.isEmpty() ?
+                    Optional.empty() :
+                    Optional.of(topologicalSet.first());
+            }
+            public boolean hasFirstToRemove() {
+                return topologicalFirst().map(Node::hasBefore).orElse(false);
+            }
+            public void removeFirst() {
+                topologicalFirst().ifPresent(this::remove);
+            }
+
+            public void remove(Node nodeToRemove) {
+                final int idToRemove = nodeToRemove.id;
+                BitSet nodesToUpdate = new BitSet();
+                nodeToRemove.before.stream()
+                    .filter(id -> id != idToRemove)
+                    .mapToObj(this::node)
+                    .forEach(nodeBefore -> {
+                        topologicalSet.remove(nodeBefore);
+                        nodeBefore.after.clear(idToRemove);
+                        nodesToUpdate.set(nodeBefore.id);
+                    });
+                nodeToRemove.after.stream()
+                    .filter(id -> id != idToRemove)
+                    .mapToObj(this::node)
+                    .forEach(nodeAfter -> {
+                        topologicalSet.remove(nodeAfter);
+                        nodeAfter.before.clear(idToRemove);
+                        nodesToUpdate.set(nodeAfter.id);
+                    });
+                nodesToUpdate.stream()
+                    .mapToObj(this::node)
+                    .forEach(topologicalSet::add);
+                topologicalSet.remove(nodeToRemove);
+                nodes[idToRemove] = null;
+                size--;
+            }
+
+            @Override
+            public String toString() {
+                return Arrays.stream(nodes)
+                    .filter(Objects::nonNull)
+                    .map(Node::toString)
+                    .collect(Collectors.joining(
+                        System.lineSeparator(),
+                        String.format("---- %d courses: ----%n", size),
+                        String.format("%n.....................%n")
+                            + dumpTopology() +
+                        String.format("%n---------------------%n")
+                    ));
+            }
+            private String dumpTopology() {
+                return topologicalSet.stream()
+                    .map(Node::id)
+                    .toList().toString();
+            }
+        }
+    }
+}
---
+++ puzzles-leet-code/src/test/java/org/krmdemo/techlabs/leet_code_0000_1000/TestCase_207__Course_Schedule.java
@@ -0,0 +1,78 @@
+package org.krmdemo.techlabs.leet_code_0000_1000;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+/**
+ * Test-Case for Leet-Code puzzle {@link Problem_207__Course_Schedule}
+ */
+public class TestCase_207__Course_Schedule {
+
+    private final Problem_207__Course_Schedule sln =
+        Problem_207__Course_Schedule.Solution.DEFAULT;
+
+    @Test
+    void test_ex_01() {
+        int[][] prerequisites = new int[][] { { 1, 0 } };
+        assertThat(sln.canFinish(2, prerequisites)).isTrue();
+    }
+
+    @Test
+    void test_ex_02() {
+        int[][] prerequisites = new int[][] { { 1, 0 }, { 0, 1 } };
+        assertThat(sln.canFinish(2, prerequisites)).isFalse();
+    }
+
+    @Test
+    void test_no_edges() {
+        assertThat(sln.canFinish(2, null)).isTrue();
+        assertThat(sln.canFinish(2, new int[][]{})).isTrue();
+        assertThatIllegalArgumentException().isThrownBy(
+            () -> sln.canFinish(0, null)
+        ).withMessageContaining("must be positive");
+    }
+
+    @Test
+    void test_edges_tree() {
+        int[][] prerequisites = new int[][] {
+            { 0, 1 }, { 0, 2 }, { 0, 3 },
+            { 1, 10 }, { 1, 11 }, { 1, 12 },
+            { 2, 21 }, { 2, 22 }, { 2, 23 },
+            { 3, 31 }, { 3, 32 }, { 3, 33 },
+        };
+        assertThat(sln.canFinish(34, prerequisites)).isTrue();
+
+        prerequisites = Stream.concat(
+            Arrays.stream(prerequisites),
+            Arrays.stream(new int[][] {
+                {11, 22}, {22, 33}, {33, 11}  // <-- a loop of edges
+            })
+        ).toArray(int[][]::new);
+        assertThat(sln.canFinish(34, prerequisites)).isFalse();
+    }
+
+    @Test
+    void test_diamond_levels() {
+        int[][] prerequisites = new int[][] {
+            { 0, 1 }, { 0, 2 }, { 0, 3 },
+            { 1, 11 }, { 1, 12 }, { 1, 13 },
+            { 2, 11 }, { 2, 12 }, { 2, 13 },
+            { 3, 11 }, { 3, 12 }, { 3, 13 },
+            { 11, 21 }, { 11, 22 }, { 11, 23 },
+            { 12, 21 }, { 12, 22 }, { 12, 23 },
+            { 13, 21 }, { 13, 22 }, { 13, 23 },
+            { 21, 30 }, { 22, 30 }, { 23, 30 },
+        };
+        assertThat(sln.canFinish(31, prerequisites)).isTrue();
+
+        assertThat(prerequisites[16]).containsExactly(12, 22);
+        prerequisites[16][1] = 3;  // <-- change the existing edge
+        assertThat(prerequisites[16]).containsExactly(12, 3);
+        assertThat(sln.canFinish(31, prerequisites)).isFalse();
+    }
+}
